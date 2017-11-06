7c7a11eb1a6cb1ae71f78e038561d08086b19429
bjlag <vlad.duplin@ya.ru>
2017-11-06 14:43:44 +0300

    Добавил параметр library, devtool и watch

--- lesson_2/webpack.config.js
+++ lesson_2/webpack.config.js
@@ -6,6 +6,14 @@ module.exports = {
     entry: path.join( __dirname, 'src', 'app', 'index.js' ),
     output: {
         path: path.join( __dirname, 'dist' ),
-        filename: 'build.js'
+        filename: 'build.js',
+        library: 'lib'
+    },
+
+    devtool: 'cheap-inline-module-source-map',
+
+    watch: true,
+    watchOptions: {
+        aggregateTimeout: 100
     }
 };
