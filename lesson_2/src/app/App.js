7571571fb08e1d9102644ceaeb2f5fae71e68469
bjlag <vlad.duplin@ya.ru>
2017-11-07 18:37:01 +0300

    Подключил модули StudentInfo и ClickButton

--- lesson_2/src/app/App.js
+++ lesson_2/src/app/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const StudentInfo = require( './components/StudentInfo' );
+const ClickButton = require( './components/ClickButton' );
+
 class App extends React.Component {
     render() {
         return <h1>Приложение на ReactJS</h1>;
@@ -8,3 +11,5 @@ class App extends React.Component {
 }
 
 ReactDOM.render( <App/>, document.getElementById( 'root' ) );
+ReactDOM.render( <StudentInfo/>, document.getElementById( 'student' ) );
+ReactDOM.render( <ClickButton/>, document.getElementById( 'button' ) );
