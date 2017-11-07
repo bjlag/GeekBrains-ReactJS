4117050407333e3ad9446bcc1144662c316228e6
bjlag <vlad.duplin@ya.ru>
2017-11-07 19:07:50 +0300

    Добавил функцию getFormattedDate, которая выводит отформатированную текущую дату. При нажатии на кнопку выполняетя эта функция.

--- lesson_2/src/app/components/ClickButton.js
+++ lesson_2/src/app/components/ClickButton.js
@@ -5,12 +5,20 @@ class ClickButton extends React.Component {
         super();
     }
 
-    press() {
-        alert( 'Нажата кнопка' );
+    getFormattedDate() {
+        const dateNow = new Date();
+
+        let day = ( dateNow.getDate() < 9 ? '0' + dateNow.getDate() : dateNow.getDate() );
+        let month = ( dateNow.getMonth() + 1 < 9 ? '0' + dateNow.getMonth() + 1 : dateNow.getMonth() + 1 );
+        let year = dateNow.getFullYear();
+
+        alert( day + '.' + month + '.' + year );
+
+        return true;
     }
 
     render() {
-        return <button onClick={this.press}>Нажми</button>
+        return <button onClick={this.getFormattedDate}>Нажми</button>
     }
 }
 
