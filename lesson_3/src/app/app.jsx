1705a57bb93a94d136e9b0f1df18646deed59d1a
bjlag <vlad.duplin@ya.ru>
2017-11-10 18:14:17 +0300

    initial lesson_3

---
+++ lesson_3/src/app/app.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+const app = document.getElementById( 'app' );
+
+ReactDOM.render( (
+    <div id={'wrapper'} className={'clearfix'}>
+        123
+    </div>
+), app );
---
+++ lesson_3/webpack.config.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const path = require( 'path' );
+
+module.exports = {
+    entry: {
+        app: path.join( __dirname, 'src', 'app', 'app' )
+    },
+
+    output: {
+        path: path.join( __dirname, 'dist' ),
+        filename: '[name].js'
+    },
+
+    resolve: {
+        extensions: [ '.js', '.jsx', '.css' ]
+    },
+
+    devtool: 'cheap-inline-module-source-map',
+
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            }
+        ]
+    }
+};
