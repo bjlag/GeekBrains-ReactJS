83fd5f23047ec277f90b448d4af6b6936bbfc8b2
bjlag <vlad.duplin@ya.ru>
2017-11-10 22:56:07 +0300

    Вывод компонет шапки, заголовка, контента и футера

--- lesson_3/src/app/app.jsx
+++ lesson_3/src/app/app.jsx
@@ -1,10 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+
+import Header from './components/Header';
+import PageTitle from './components/PageTitle';
+import Content from './components/Content';
+import Footer from './components/Footer';
 
 const app = document.getElementById( 'app' );
 
 ReactDOM.render( (
-    <div id={'wrapper'} className={'clearfix'}>
-        123
+    <div id="wrapper" className="clearfix">
+        <Header/>
+        <PageTitle title={'Blog'} description={'Our Latest News'}/>
+        <Content/>
+        <Footer/>
     </div>
 ), app );
