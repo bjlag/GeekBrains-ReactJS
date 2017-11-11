54bfcf085c8772d2a7c5a1d309af4bbc7828f418
bjlag <vlad.duplin@ya.ru>
2017-11-12 00:29:25 +0300

    Перенес компоненты для вывода главной страницы блога в отдельный layout

--- lesson_4/src/app/app.jsx
+++ lesson_4/src/app/app.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Header from './components/Header';
-import PageTitle from './components/PageTitle';
-import Content from './components/Content';
-import Footer from './components/Footer';
+import BlogList from './layouts/BlogList';
 
 const app = document.getElementById( 'app' );
 
-ReactDOM.render( (
-    <div id="wrapper" className="clearfix">
-        <Header/>
-        <PageTitle title={'Blog'} description={'Our Latest News'}/>
-        <Content/>
-        <Footer/>
-    </div>
-), app );
+ReactDOM.render(
+    <BlogList/>
+, app );
