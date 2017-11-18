680c99464cedb8e7093cfc4fc689e4c5c4b7f3ff
bjlag <vlad.duplin@ya.ru>
2017-11-18 16:56:49 +0300

    Настройки перенес в state Ajax загрузка меню

--- lesson_5/src/app/layouts/Main.js
+++ lesson_5/src/app/layouts/Main.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
@@ -10,49 +11,44 @@ export default class Main extends Component {
     constructor() {
         super( ...arguments );
 
-        this.options = {
+        this.state = {
             logo: {
                 standard: '/images/logo.png',
                 retina: '/images/logo@2x.png',
                 alt: 'Блог'
             },
 
-            menu: [
-                {
-                    name: 'Блог',
-                    href: '/'
-                },
-                {
-                    name: 'Блог с ошибкой',
-                    href: '/blog-error/'
-                },
-                {
-                    name: 'Портфолио',
-                    href: '/portfolio/'
-                }
-            ]
+            menuJson: '../data/menu.json',
+
+            menu: []
         };
 
-        this.menuItems = this.options.menu.map( ( item, index ) => {
-            return (
-                <MenuItem href={ item.href } key={ index }>{ item.name }</MenuItem>
-            );
-        } );
+        axios.get( this.state.menuJson )
+            .then( ( response ) => {
+                const { data } = response;
 
+                const menuItems = data.map( ( item, index ) => {
+                    return (
+                        <MenuItem href={ item.href } key={ index }>{ item.name }</MenuItem>
+                    );
+                } );
 
+                this.setState( {
+                    menu: menuItems
+                } );
+            } );
     }
 
     render() {
         return (
             <div id="wrapper" className="clearfix">
                 <Header>
-                    <Logo srcLogoStandard={ this.options.logo.standard }
-                          srcLogoRetina={ this.options.logo.retina } alt={ this.options.logo.alt }/>
+                    <Logo srcLogoStandard={ this.state.logo.standard }
+                          srcLogoRetina={ this.state.logo.retina } alt={ this.state.logo.alt }/>
                     <Menu>
-                        { this.menuItems }
+                        { this.state.menu }
                     </Menu>
                 </Header>
-
 
                 { this.props.children }
 
