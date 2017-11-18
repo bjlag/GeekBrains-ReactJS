c655ebd01b99a2f435cf5d0b14cd72978f4e8b73
bjlag <vlad.duplin@ya.ru>
2017-11-18 16:24:46 +0300

    Улучшение читаемости кода

--- lesson_5/src/app/components/Logo.js
+++ lesson_5/src/app/components/Logo.js
@@ -5,8 +5,8 @@ export default class Logo extends Component {
     render() {
         return (
             <div id="logo">
-                <Link to="/" className="standard-logo"><img src={this.props.srcLogoStandard} alt={this.props.alt}/></Link>
-                <Link to="/" className="retina-logo"><img src={this.props.srcLogoRetina} alt={this.props.alt}/></Link>
+                <Link to="/" className="standard-logo"><img src={ this.props.srcLogoStandard } alt={ this.props.alt }/></Link>
+                <Link to="/" className="retina-logo"><img src={ this.props.srcLogoRetina } alt={ this.props.alt }/></Link>
             </div>
         );
     }
