7803c5b64a3c0b1214e9c28e09e65d8248679bfb
bjlag <vlad.duplin@ya.ru>
2017-11-18 20:02:44 +0300

    Изменил роутинг в соответствии новой структуры меню

--- lesson_5/src/app/app.jsx
+++ lesson_5/src/app/app.jsx
@@ -2,18 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
-import LayoutMain from './layouts/Main';
+import Main from './layouts/Main';
+import Index from './pages/Index';
 import Blog from './pages/Blog';
-import Portfolio from './pages/Portfolio';
+import Comments from './pages/Comments';
+import Users from './pages/Users';
 import Error404 from './pages/Error404';
 
 const app = document.getElementById( 'app' );
 
 ReactDOM.render(
     <Router history={ browserHistory }>
-        <Route path="/" component={ LayoutMain }>
-            <IndexRoute component={ Blog }/>
-            <Route path="portfolio/" component={ Portfolio }/>
+        <Route path="/" component={ Main }>
+            <IndexRoute component={ Index }/>
+            <Route path="blog/" component={ Blog }/>
+            <Route path="comments/" component={ Comments }/>
+            <Route path="users/" component={ Users }/>
             <Route path="*" component={ Error404 }/>
         </Route>
     </Router>
